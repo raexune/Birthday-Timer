1d9fc0e84010bc1af3a6704c99473b6c9f709ec9
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2022-06-02 08:36:17 +0200

    added hours and minutes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getTimeUntilBirthday() {
+function getDays() {
     let endDate = new Date("August 10, 2022 00: 00: 00").getTime();
     let startDate = new Date().getTime();
     let timeUntil = endDate - startDate;
@@ -6,5 +6,29 @@ function getTimeUntilBirthday() {
     return daysUntil;
 }
 
-const timer = document.querySelector(".timer");
-timer.innerHTML = getTimeUntilBirthday();
+function getHours() {
+    let endDate = new Date("August 10, 2022 00: 00: 00").getTime();
+    let startDate = new Date().getTime();
+    let timeUntil = endDate - startDate;
+    let hoursUntil = Math.floor(timeUntil / 3600000 - Math.floor(timeUntil / 86400000) * 24);
+    return hoursUntil;
+}
+
+function getMinutes() {
+    let endDate = new Date("August 10, 2022 00: 00: 00").getTime();
+    let startDate = new Date().getTime();
+    let timeUntil = endDate - startDate;
+    let minutesUntil = Math.floor((timeUntil / 60000) - (Math.floor(timeUntil / 86400000) * 1440) - (Math.floor(timeUntil / 3600000 - Math.floor(timeUntil / 86400000) * 24) * 60));
+    return minutesUntil;
+}
+
+
+
+const days = document.querySelector(".days");
+days.innerHTML = getDays();
+
+const hours = document.querySelector(".hours");
+hours.innerHTML = getHours();
+
+const minutes = document.querySelector(".minutes");
+minutes.innerHTML = getMinutes();
