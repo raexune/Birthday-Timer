ed91b00e3424e5c4b7fac846ee4eca01346f2339
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2023-11-06 22:02:46 +0100

    updated for 2024

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 function getDays() {
-    let endDate = new Date("August 10, 2022 00: 00: 00").getTime();
-    let startDate = new Date().getTime();
-    let timeUntil = endDate - startDate;
-    let daysUntil = Math.floor(timeUntil / 86400000);
-    return daysUntil;
+  let endDate = new Date("August, 2024 10 00: 00: 00").getTime();
+  let startDate = new Date().getTime();
+  let timeUntil = endDate - startDate;
+  let daysUntil = Math.floor(timeUntil / 86400000);
+  return daysUntil;
 }
 
 function getHours() {
-    let endDate = new Date("August 10, 2022 00: 00: 00").getTime();
-    let startDate = new Date().getTime();
-    let timeUntil = endDate - startDate;
-    let hoursUntil = Math.floor(timeUntil / 3600000 - Math.floor(timeUntil / 86400000) * 24);
-    return hoursUntil;
+  let endDate = new Date("August 10, 2024 00: 00: 00").getTime();
+  let startDate = new Date().getTime();
+  let timeUntil = endDate - startDate;
+  let hoursUntil = Math.floor(
+    timeUntil / 3600000 - Math.floor(timeUntil / 86400000) * 24
+  );
+  return hoursUntil;
 }
 
 function getMinutes() {
-    let endDate = new Date("August 10, 2022 00: 00: 00").getTime();
-    let startDate = new Date().getTime();
-    let timeUntil = endDate - startDate;
-    let minutesUntil = Math.floor((timeUntil / 60000) - (Math.floor(timeUntil / 86400000) * 1440) - (Math.floor(timeUntil / 3600000 - Math.floor(timeUntil / 86400000) * 24) * 60));
-    return minutesUntil;
+  let endDate = new Date("August 10, 2024 00: 00: 00").getTime();
+  let startDate = new Date().getTime();
+  let timeUntil = endDate - startDate;
+  let minutesUntil = Math.floor(
+    timeUntil / 60000 -
+      Math.floor(timeUntil / 86400000) * 1440 -
+      Math.floor(timeUntil / 3600000 - Math.floor(timeUntil / 86400000) * 24) *
+        60
+  );
+  return minutesUntil;
 }
-
-
 
 const days = document.querySelector(".days");
 days.innerHTML = getDays();
